42b73c2ce23c08d078d348acf2d5bc201fd039ec
Nikhil Gupta <156237828+nikgup11@users.noreply.github.com>
2026-03-11 23:08:28 -0400

    Pull d3 CSV data in main.js
    SR_TYPE and csv file will likely need to be changed in the future when we decide on a data attribute to focus on

---
+++ js/main.js
@@ -0,0 +1,16 @@
+d3.csv('data/311Sample.csv') // Might be replaced with a new preprocessed CSV for specific attr
+.then(data => {
+    console.log("number of items: " + data.length);
+
+    data.forEach(d => {
+      d.latitude = +d.latitude; 
+      d.longitude = +d.longitude;  
+      // d.SR_TYPE = d.SR_TYPE; // Get the service type - will need to preprocess 311Sample first after deciding which attr to go with
+    });
+
+    // Initialize map and then show it
+    leafletMap = new LeafletMap({ parentElement: '#my-map'}, data);
+
+
+  })
+  .catch(error => console.error(error));
