2bfdebbe955a3810679fe10f0153a1d5cc11613d
Jack Detrick <98991308+JackDetrick66@users.noreply.github.com>
2026-03-26 02:25:01 -0400

    Update leafletMap.js

--- js/leafletMap.js
+++ js/leafletMap.js
@@ -76,11 +76,6 @@ class LeafletMap {
     vis.openStreetUrl = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
     vis.openStreetAttr = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
 
-    vis.colorScale = d3.scaleOrdinal()
-      .domain([...new Set(vis.data.map(d => d.DEPT_NAME))])
-      .range(d3.schemeTableau10);
-
-    //this is the base map layer, where we are showing the map background
     vis.base_layer = L.tileLayer(vis.openStreetUrl, {
       id: 'openStreet-image',
       attribution: vis.openStreetAttr,
@@ -99,51 +94,6 @@ class LeafletMap {
     vis.overlay = d3.select(vis.theMap.getPanes().overlayPane);
     vis.svg = vis.overlay.select('svg').attr("pointer-events", "auto");
 
-    vis.Dots = vis.svg.selectAll('circle')
-                .data(vis.data) 
-                .join('circle')
-                    .attr("fill", d => vis.colorScale(d.DEPT_NAME))  //---- TO DO- color by magnitude 
-                    .attr("stroke", "black")
-                    //Leaflet has to take control of projecting points. 
-                    //Here we are feeding the latitude and longitude coordinates to
-                    //leaflet so that it can project them on the coordinates of the view. 
-                    //the returned conversion produces an x and y point. 
-                    //We have to select the the desired one using .x or .y
-                    .attr("cx", d => vis.theMap.latLngToLayerPoint([d.LATITUDE,d.LONGITUDE]).x)
-                    .attr("cy", d => vis.theMap.latLngToLayerPoint([d.LATITUDE,d.LONGITUDE]).y) 
-                    .attr("r", d=> 8)  // --- TO DO- want to make radius proportional to earthquake size? 
-                    .on('mouseover', function(event,d) { //function to add mouseover event
-                        d3.select(this).transition() //D3 selects the object we have moused over in order to perform operations on it
-                          .duration('150') //how long we are transitioning between the two states (works like keyframes)
-                          .attr("fill", "red") //change the fill
-                          .attr('r', 8); //change radius
-
-                        //create a tool tip
-                        d3.select('#tooltip')
-                            .style('opacity', 1)
-                            .style('z-index', 1000000)
-                              // Format number with million and thousand separator
-                              //***** TO DO- change this tooltip to show useful information about the quakes
-                            .html(`<div class="tooltip-label">Description: ${d.SR_TYPE_DESC}, Priority: ${d.PRIORITY || 'No Priority'}, Date of Call: ${d.DATE_CREATED}, Date Closed: ${d.DATE_CLOSED}, Department: ${d.DEPT_NAME} `);
-
-                      })
-                    .on('mousemove', (event) => {
-                        //position the tooltip
-                        d3.select('#tooltip')
-                          .style('left', (event.pageX + 10) + 'px')   
-                          .style('top', (event.pageY + 10) + 'px');
-                      })              
-                    .on('mouseleave', function() { //function to add mouseover event
-                        d3.select(this).transition() //D3 selects the object we have moused over in order to perform operations on it
-                          .duration('150') //how long we are transitioning between the two states (works like keyframes)
-                          .attr("fill", "steelblue") //change the fill  TO DO- change fill again
-                          .attr('r', 8) //change radius
-
-                        d3.select('#tooltip').style('opacity', 0);//turn off the tooltip
-
-                          })
-    
-    //handler here for updating the map, as you zoom in and out           
     // Redraw points when zooming in and out
     vis.theMap.on("zoomend", function(){
       vis.updateVis();
@@ -199,7 +149,7 @@ class LeafletMap {
     vis.updateVis();
   }
 
- setBackground(selectedBackground, selectedAttr) {
+  setBackground(selectedBackground, selectedAttr) {
   let vis = this;
 
   // Remove the current tile layer from the map
@@ -221,18 +171,6 @@ class LeafletMap {
     vis.Dots = vis.svg.selectAll('circle')
         .data(vis.data)
         .join('circle')
-
-            .attr("cx", d => vis.theMap.latLngToLayerPoint([d.LATITUDE, d.LONGITUDE]).x)
-            .attr("cy", d => vis.theMap.latLngToLayerPoint([d.LATITUDE, d.LONGITUDE]).y)
-            .attr("r", 8);
-}
-
-
-  renderVis() {
-    let vis = this;
-
-    //not using right now... 
- 
             // Apply the current color state during any redraw/zoom
             .attr("fill", d => vis.getPointColor(d)) 
             .attr("stroke", "black")
