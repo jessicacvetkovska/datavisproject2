49ec46ec5fe8cc83b67c89cec44d964f98655a7d
Viscount <jackdetrick6@gmail.com>
2026-03-30 14:03:38 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -104,7 +104,7 @@ d3.csv('js/data/311Sample.csv').then(data => {
     });
 
     d3.select('#btn-dark').on('click', () => {
-        leafletMap.setBackground('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png');
+        leafletMap.setBackground('hhttps://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png');
     });
 
     // Initial render call to populate all charts
